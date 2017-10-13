6d90781239cb7e7916b5a618a3b73e8d591c7c84
150454 <Jeremy.DUMONT@student.umons.ac.be>
2017-10-13 15:53:27 +0200

    j'ai théoriquement fini la partie 1

--- src/main/java/exercise1/Exercise1.java
+++ src/main/java/exercise1/Exercise1.java
@@ -13,6 +13,11 @@ public class Exercise1 {
 	public static void main(String[] args) {
 		// Considérant que cette classe est démarrée en ligne de commande avec un premier paramètre donnant le nom
 		// et un second donnant l'âge (nombre entier), créez un objet Person sur base de ceux-ci.
+		String nom=new String();
+		nom="Jérémy";
+		int age=20;
+		Person moi=new Person(age,nom);
 		// Ecrivez ensuite le nom et l'âge de cette personne sur la sortie standard.
+		System.out.println("Je m'apelle "+moi.getName()+" et ai "+moi.getAge()+" ans.");
 	}
 }
